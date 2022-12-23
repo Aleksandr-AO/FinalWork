33c4ed51836c907d3e32a091c95b73b48bd0a330
Aleksandr <lepalovskiy2015@mail.ru>
2022-12-23 18:02:03 +0700

    Создал метод показ массив

--- Program.cs
+++ Program.cs
@@ -24,3 +24,13 @@ void Diffarray(string[] arr)
         else arr[i] =  string.Empty;
     }
 }
+
+void PrintArray(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
